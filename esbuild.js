4ea90e9843c5db1545a33232b0afdf5b4d9a0eaf
gohitx <gohit21@gmail.com>
2026-03-31 03:10:30 -0500

    🦄 [chore] remove NeteaseCloudMusicApi dependency and bump version to 1.2.5

--- esbuild.js
+++ esbuild.js
@@ -52,7 +52,7 @@ async function main() {
 		sourcesContent: false,
 		platform: 'node',
 		outfile: 'dist/extension.js',
-		external: ['vscode', 'NeteaseCloudMusicApi'],
+		external: ['vscode'],
 		logLevel: 'silent',
 		plugins: [extensionEsbuildProblemMatcherPlugin],
 	});
