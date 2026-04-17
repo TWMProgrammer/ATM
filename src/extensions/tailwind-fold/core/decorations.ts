218c33d4a9f750cfceaf144f897551ea8bf1242c
gohitx <gohit21@gmail.com>
2026-04-17 18:25:52 -0500

    ⚙️ [fix] change cursor normal por cursor pinter

--- src/extensions/tailwind-fold/core/decorations.ts
+++ src/extensions/tailwind-fold/core/decorations.ts
@@ -26,14 +26,14 @@ export async function createFoldedDecorationType(): Promise<vscode.TextEditorDec
             contentIconPath: iconPath,
             backgroundColor: bgColor,
             margin: "0", // Removed negative margin to keep pill continuous
-            textDecoration: "none; padding: 1px 2px 1px 6px; border-radius: 6px 0 0 6px;", // Left side inner padding and rounded border
+            textDecoration: "none; padding: 1px 2px 1px 6px; border-radius: 6px 0 0 6px; cursor: pointer;", // Left side inner padding and rounded border
         },
         after: {
             contentText: Config.get<string>(Settings.FoldedText, "..."),
             backgroundColor: bgColor, // Connect background over the text
             color: Config.get<string>(Settings.FoldedTextColor, "#7cdbfe"),
             margin: "0", // Remove margin to connect smoothly to 'before'
-            textDecoration: "none; padding: 1px 6px 1px 2px; border-radius: 0 6px 6px 0;", // Right side inner padding and rounded border
+            textDecoration: "none; padding: 1px 6px 1px 2px; border-radius: 0 6px 6px 0; cursor: pointer;", // Right side inner padding and rounded border
         },
         textDecoration: "none; display: none;",
         // Removed global backgroundColor and borderRadius to focus solely on the joined before & after pill
