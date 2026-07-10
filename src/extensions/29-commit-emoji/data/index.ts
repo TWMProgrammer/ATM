47cf3606e4a534c498370c6a8e7081b976d38344
gohitx <gohit21@gmail.com>
2026-07-10 17:12:46 -0500

    🔧 feat: add additional commit emoji options in new plus category

--- src/extensions/29-commit-emoji/data/index.ts
+++ src/extensions/29-commit-emoji/data/index.ts
@@ -3,12 +3,14 @@ import { features } from './features';
 import { fixes } from './fixes';
 import { tooling } from './tooling';
 import { misc } from './misc';
+import { plus } from './plus';
 
 const CommitEmoji: CommitEmojiEntry[] = [
 	...features,
 	...fixes,
 	...tooling,
 	...misc,
+	...plus,
 ];
 
 export default CommitEmoji;
